0f8e1fa638b5694267a2e86ce5b2a4abe5a312b8
Umut Utkan <umututkan@Umuts-MacBook-Air-2.local>
2025-06-19 14:59:24 -0700

    deleted some files

--- src/main/kotlin/com/mycorp/workflow/Pipeline.kt
+++ src/main/kotlin/com/mycorp/workflow/Pipeline.kt
@@ -5,9 +5,12 @@ import com.mycorp.workflow.ProcessOrderTask
 import com.mycorp.workflow.dsl.then
 import com.mycorp.workflow.dsl.workflow
 import software.amazon.awscdk.App
+import software.amazon.awscdk.Stack
+import software.amazon.awscdk.StackProps
 
 fun main() {
     val app = App()
+    val stack = Stack(app, "KotlinDslWorkflowStack", StackProps.builder().build())
 
     val builder = workflow {
         val processOrderTask = task("ProcessOrder", ProcessOrderTask::class)
